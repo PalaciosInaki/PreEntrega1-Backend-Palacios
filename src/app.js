76e8819183bb14a4fb8d116616e4b31ce07dcd4e
Inaki Palacios <palaciosinia@gmail.com>
2024-11-19 23:49:41 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 import productRouter from './routes/products.routes';
+import cartRouter from './routes/carts.routes';
 
 const app = express();
 const port = 8080;
@@ -8,7 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+app.get('ping', (req, res) =>{
+    res.send('pong');
+})
 
+app.use('/api/products', productRouter)
+
+app.use('/api/carts', cartRouter)
+
+app.use 
 
 
 app.listen(port, () => {
